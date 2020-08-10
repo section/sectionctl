803dfbb058117702ad02f9deecc11a56c6b79f1d
Lindsay Holmwood <lindsay@holmwood.id.au>
2020-08-10 16:49:01 +1000

    Make version number a variable, to make it re-usable

--- commands/version.go
+++ commands/version.go
@@ -5,11 +5,14 @@ import (
 	"runtime"
 )
 
+// Version is the version number of the running app
+var Version = "0.0.1"
+
 // VersionCmd handles authenticating the CLI against Section's API
 type VersionCmd struct{}
 
 // Run executes the `login` command
 func (a *VersionCmd) Run() (err error) {
-	fmt.Printf("%s (%s-%s)\n", "0.0.1", runtime.GOOS, runtime.GOARCH)
+	fmt.Printf("%s (%s-%s)\n", Version, runtime.GOOS, runtime.GOARCH)
 	return err
 }
