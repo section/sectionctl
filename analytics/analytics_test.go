fb06d1e61ac90a346df2166ca5bb3edf98feb4f2
Lindsay Holmwood <lindsay@holmwood.id.au>
2020-08-24 21:54:53 +1000

    Fall back on a machine id if git won't tell us anything

--- analytics/analytics_test.go
+++ analytics/analytics_test.go
@@ -2,6 +2,7 @@ package analytics
 
 import (
 	"github.com/stretchr/testify/assert"
+	"io/ioutil"
 	"net/http"
 	"net/http/httptest"
 	"testing"
@@ -13,13 +14,16 @@ func TestAnalyticsSubmitPostsToHeap(t *testing.T) {
 	// Setup
 	ts := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		called = true
+
+		body, _ := ioutil.ReadAll(r.Body)
+		t.Logf("%s", body)
 	}))
 	HeapBaseURI = ts.URL
 
 	// Invoke
 	e := Event{
 		Name:       "CLI invoked",
-		Properties: map[string]string{"Subcommand": "apps list"},
+		Properties: map[string]string{"subcommand": "apps list"},
 	}
 	err := Submit(e)
 
