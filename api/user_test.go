7bff174650301282943a3c2769b36ecdd1a659a8
Lindsay Holmwood <lindsay@holmwood.id.au>
2020-09-23 16:04:42 +1000

    Add ability to look up the current user

---
+++ api/user_test.go
@@ -0,0 +1,66 @@
+package api
+
+import (
+	"fmt"
+	"net/http"
+	"net/http/httptest"
+	"net/url"
+	"path/filepath"
+	"testing"
+
+	"github.com/section/section-cli/api/auth"
+	"github.com/stretchr/testify/assert"
+)
+
+func TestAPIUserReturnsRecord(t *testing.T) {
+	assert := assert.New(t)
+
+	// Setup
+	ts := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		w.WriteHeader(http.StatusOK)
+		fmt.Fprint(w, string(helperLoadBytes(t, "user.with_success.json")))
+	}))
+
+	ur, err := url.Parse(ts.URL)
+	assert.NoError(err)
+	PrefixURI = ur.String()
+
+	auth.CredentialPath = filepath.Join("auth", "testdata", "valid-credentials")
+
+	// Invoke
+	u, err := CurrentUser()
+
+	// Test
+	assert.NoError(err)
+	assert.Equal(u.ID, 1)
+	assert.Equal(u.Email, "ada@lovelace.example")
+	assert.Equal(u.FirstName, "Ada")
+	assert.Equal(u.LastName, "Lovelace")
+	assert.Equal(u.CompanyName, "Example Corp")
+	assert.Equal(u.PhoneNumber, "+13125550690")
+}
+
+func TestAPIUserHandlesErrors(t *testing.T) {
+	assert := assert.New(t)
+
+	// Setup
+	ts := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		w.WriteHeader(http.StatusTeapot)
+		fmt.Fprintf(w, "short and stout")
+	}))
+
+	ur, err := url.Parse(ts.URL)
+	assert.NoError(err)
+	PrefixURI = ur.String()
+
+	auth.CredentialPath = filepath.Join("auth", "testdata", "valid-credentials")
+
+	// Invoke
+	u, err := CurrentUser()
+
+	// Test
+	assert.Error(err)
+	assert.Equal(u, User{})
+	assert.Contains(err.Error(), "418")
+	assert.Contains(err.Error(), "short and stout")
+}
