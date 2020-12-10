1fb80b069fef803ed9a39e50457f204f92aff4b6
Lindsay Holmwood <lindsay@holmwood.id.au>
2020-12-10 13:46:01 +1100

    Make `certs` subcommand use spinner helper

--- commands/certs.go
+++ commands/certs.go
@@ -2,10 +2,8 @@ package commands
 
 import (
 	"fmt"
-	"os"
 	"time"
 
-	"github.com/briandowns/spinner"
 	"github.com/section/sectionctl/api"
 	"github.com/section/sectionctl/api/auth"
 )
@@ -23,19 +21,20 @@ type CertsRenewCmd struct {
 
 // Run executes the command
 func (c *CertsRenewCmd) Run() (err error) {
+	s := NewSpinner()
+
 	err = auth.Setup(api.PrefixURI.Host)
 	if err != nil {
 		return err
 	}
 
-	s := spinner.New(spinner.CharSets[14], 100*time.Millisecond, spinner.WithWriter(os.Stderr))
 	s.Suffix = fmt.Sprintf(" Renewing cert for %s", c.Hostname)
 	s.Start()
 	time.Sleep(2 * time.Second)
 
 	resp, err := api.DomainsRenewCert(c.AccountID, c.Hostname)
+	s.Stop()
 	if err != nil {
-		s.Stop()
 		return err
 	}
 
