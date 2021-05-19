9313cc39d8e2a985b20c28eb5c447d297e8cab26
AskAlice <alice@askalice.me>
2021-05-19 07:03:04 -0600

    don't repeat yourself

--- commands/helpers.go
+++ commands/helpers.go
@@ -25,12 +25,15 @@ func IsInCtxBool(ctx context.Context, arg string) bool {
 
 // NewSpinner returns a nicely formatted spinner for display while users are waiting.
 func NewSpinner(ctx context.Context, txt string) (s *spinner.Spinner) {
+	var writer io.Writer
 	if IsInCtxBool(ctx, "quiet"){
-		s = spinner.New(spinner.CharSets[14], 100*time.Millisecond, spinner.WithWriter(io.Discard))
-	} else {
-		s = spinner.New(spinner.CharSets[14], 100*time.Millisecond, spinner.WithWriter(os.Stderr))
+		writer = io.Discard
+	}else{
+		writer = os.Stderr
 	}
+	s = spinner.New(spinner.CharSets[35], 500*time.Millisecond, spinner.WithWriter(writer))
+	s.Color("cyan")
 	s.Prefix = fmt.Sprintf("%s... ", txt)
-	s.FinalMSG = fmt.Sprintf("%s... done\n", txt)
+	s.FinalMSG = fmt.Sprintf("%s... ✔️\n", txt)
 	return s
 }
